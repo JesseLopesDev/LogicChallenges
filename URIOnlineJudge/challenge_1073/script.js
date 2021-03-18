cc718994326fb13ce33ea7ed7a27adc760a7a3c2
JesseLopesDev <jesselopes.dev@gmail.com>
2021-03-18 13:47:27 -0300

    Resolvendo o desafio 1073

---
+++ URIOnlineJudge/challenge_1073/script.js
@@ -0,0 +1,25 @@
+const input = require('fs').readFileSync('./stdin', 'utf8');
+const value = input.split('\n');
+
+const valor = parseInt(value.shift());
+
+let numeroPar = 0;
+let valorCalculado;
+
+for (let index = 0; index <= valor; index++) {
+
+  if (valor % 2 == 0) {
+    numeroPar += 2;
+    valorCalculado = Math.pow(numeroPar, 2);
+
+    if (numeroPar <= valor) {
+
+      if (numeroPar == 4) {
+        console.log(`${numeroPar} ^ ${2} = ${valorCalculado}`);
+        continue;
+      }
+
+      console.log(`${numeroPar} ^ ${numeroPar} = ${valorCalculado}`);
+    }
+  }
+}
